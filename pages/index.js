c4127fd04bb95a0dc1feea88f563f16c947df2bf
Jungmin Lee <whljm1003@gmail.com>
2022-06-24 16:41:14 +0900

    2.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "ddc787342ec23589c7fd7628051f7842";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title={"Home"} />
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movies) => (
+        <div key={movies.id}>
+          <h4>{movies.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
